b4b4aa2c6bc36d2929065a7d33aa94dce7e7252a
SUMIDA Yuki <sumida.yuki@f-inet.co.jp>
2023-06-30 15:10:10 +0900

    varの部分をconstに書き直し

--- github.ts
+++ github.ts
@@ -2,12 +2,12 @@
 const githubToken: string = userProperties.getProperty('GITHUB_TOKEN') || '';
 
 function getGithubRanking(): string {
-    var headers = {
+    const headers = {
       'Authorization': `Bearer ${githubToken}`,
       'Accept': 'application/vnd.github+json',
       'X-GitHub-Api-Version': '2022-11-28'
     };
-    var options = {
+    const options = {
         'method' : 'get',
         'headers' : headers
     };
@@ -31,52 +31,46 @@ function getGithubRanking(): string {
     return rankingStrings.join('\n');
 }
 
-function correctPr(options, map) {
+function correctPr(options, map: Map<string, number>) {
     // PRの一覧取得
   // TODO: 対象のリポジトリをいい感じに編集できるようにする
-  var response = UrlFetchApp.fetch('https://api.github.com/repos/kotanin/TestActions/pulls?state=all', options);
-    var json = response.getContentText();
-    var data = JSON.parse(json);
+  const response = UrlFetchApp.fetch('https://api.github.com/repos/kotanin/TestActions/pulls?state=all', options);
+    const json = response.getContentText();
+    const data = JSON.parse(json);
 
-    var today = Date.now()
+    const today = Date.now()
     data.forEach(pr => {
       // 7日以内のPRを対象に集計する
-      var diffDays = (today - Date.parse(pr['updated_at'])) / 86400000;
+      const diffDays = (today - Date.parse(pr['updated_at'])) / 86400000;
       if (diffDays <= 7) {
         // PRを立てた人を集計する
-        var userName:string = pr['user']['login'];
-        var count = map.get(userName);
-        if (count == undefined) {
-          count = 0;
-        }
-        map.set(userName, ++count);
+        const userName:string = pr['user']['login'];
+        const count = map.get(userName) || 0;
+        map.set(userName, count + 1);
 
         // 7日以内にコメントした人を集計する
-        var commentResponse = UrlFetchApp.fetch(pr['comments_url'], options);
-        var commentJson = commentResponse.getContentText();
-        var commentData = JSON.parse(commentJson);
+        const commentResponse = UrlFetchApp.fetch(pr['comments_url'], options);
+        const commentJson = commentResponse.getContentText();
+        const commentData = JSON.parse(commentJson);
         commentData.forEach(comment => {
           if ((today - Date.parse(comment['updated_at'])) / 86400000 <= 7) {
-            var commentUserName = comment['user']['login'];
-            var commentCount = map.get(commentUserName);
-            if (commentCount == undefined) {
-              commentCount = 0;
-            }
-            map.set(commentUserName, ++commentCount);
+            const commentUserName = comment['user']['login'];
+            const commentCount = map.get(commentUserName) || 0;
+            map.set(commentUserName, commentCount + 1);
           }
         });
       }
     });
 }
 
-function correctIssue(options, map) {
+function correctIssue(options, map: Map<string, number>) {
   // Issueの一覧取得
   // TODO: 対象のリポジトリをいい感じに編集できるようにする
-  var response = UrlFetchApp.fetch('https://api.github.com/repos/kotanin/TestActions/issues?state=all', options);
-  var json = response.getContentText();
-  var data = JSON.parse(json);
+  const response = UrlFetchApp.fetch('https://api.github.com/repos/kotanin/TestActions/issues?state=all', options);
+  const json = response.getContentText();
+  const data = JSON.parse(json);
 
-  var today = Date.now()
+  const today = Date.now()
   data.forEach(issue => {
     // issueにはPRが含まれるので、それを弾く
     if (issue['pull_request'] != undefined) {
@@ -84,28 +78,22 @@ function correctIssue(options, map) {
     }
 
     // 7日以内のIssueを対象に集計する
-    var diffDays = (today - Date.parse(issue['updated_at'])) / 86400000;
+    const diffDays = (today - Date.parse(issue['updated_at'])) / 86400000;
     if (diffDays <= 7) {
       // PRを立てた人を集計する
-      var userName:string = issue['user']['login'];
-      var count = map.get(userName);
-      if (count == undefined) {
-        count = 0;
-      }
-      map.set(userName, ++count);
+      const userName:string = issue['user']['login'];
+      const count = map.get(userName) || 0;
+      map.set(userName, count + 1);
 
       // 7日以内にコメントした人を集計する
-      var commentResponse = UrlFetchApp.fetch(issue['comments_url'], options);
-      var commentJson = commentResponse.getContentText();
-      var commentData = JSON.parse(commentJson);
+      const commentResponse = UrlFetchApp.fetch(issue['comments_url'], options);
+      const commentJson = commentResponse.getContentText();
+      const commentData = JSON.parse(commentJson);
       commentData.forEach(comment => {
         if ((today - Date.parse(comment['updated_at'])) / 86400000 <= 7) {
-          var commentUserName = comment['user']['login'];
-          var commentCount = map.get(commentUserName);
-          if (commentCount == undefined) {
-            commentCount = 0;
-          }
-          map.set(commentUserName, ++commentCount);
+          const commentUserName = comment['user']['login'];
+          const commentCount = map.get(commentUserName) || 0;
+          map.set(commentUserName, commentCount + 1);
         }
       });
     }
